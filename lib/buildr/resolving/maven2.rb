431dd224e0897132ad9801f030c899367f296435
Antoine Toulme <antoine@lunar-ocean.com>
2010-06-16 13:49:56 -0700

    base for resolvers

---
+++ lib/buildr/resolving/maven2.rb
@@ -0,0 +1,35 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with this
+# work for additional information regarding copyright ownership.  The ASF
+# licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+module Buildr
+  
+  class Maven2Resolver < Buildr::Resolver
+    def initialize()
+      super(:mvn)
+    end
+    
+    def resolve #TODO
+      artifacts << ["com.example:foo:jar:1.1", "com.example:bar:jar:1.1"]
+      artifacts.flatten!
+    end
+    
+    class << self
+      def apply?(project) #TODO
+        File.exist?(project.path_to("pom.xml"))
+      end
+    end
+  end
+  
+end
